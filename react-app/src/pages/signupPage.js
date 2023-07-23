45c62de0c54f150dc6167b353b5feb85c30d3fc5
Justine <ujusine@yahoo.com>
2023-07-23 19:42:45 +0800

    import signup form

---
+++ react-app/src/pages/signupPage.js
@@ -0,0 +1,11 @@
+import SignupForm from "../components/credentials/signupForm";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default SignupPage;
