0db872becec4c40d26fb26f52ba4b3c1381c7183
Justine <ujusine@yahoo.com>
2023-07-25 16:45:32 +0800

    single post page

---
+++ react-app/src/pages/postPage.js
@@ -0,0 +1,11 @@
+import Post from "../components/posts/allPosts/id/post";
+
+const PostPage = () => {
+  return (
+    <div className="home">
+      <Post />
+    </div>
+  );
+};
+
+export default PostPage;
