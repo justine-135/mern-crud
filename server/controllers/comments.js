15cc9084817f4ccc07d357196f475b25f58a6514
Justine <ujusine@yahoo.com>
2023-07-25 16:42:42 +0800

    comments controller

---
+++ server/controllers/comments.js
@@ -0,0 +1,36 @@
+const Comments = require("../models/comments");
+
+const create = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const comments = await Comments.create({
+      comment: req.body.comment,
+      post: req.body._id,
+      email: req.user.email,
+    });
+
+    res.status(200).json(comments);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const indexPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comment = await Comments.find({ post: id });
+    // const comment = await Comments.findOne({
+    //   post: req.posts._id,
+    // });
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  create,
+  indexPost,
+};
