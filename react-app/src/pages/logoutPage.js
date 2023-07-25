94520c95404cf810a365864ee667b78675de2805
Justine <ujusine@yahoo.com>
2023-07-25 16:45:20 +0800

    logout page

--- react-app/src/pages/logoutPage.js
+++ react-app/src/pages/logoutPage.js
@@ -2,23 +2,35 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
 
-const LogoutPage = () => {
+const LogoutPage = ({ loggedIn, setLoggedIn }) => {
   const navigate = useNavigate();
+
   useEffect(() => {
-    logout();
+    if (!loggedIn) {
+      navigate("/login");
+    }
   }, []);
 
-  const logout = async () => {
-    await axios
-      .get("/api/logout")
-      .then(function (response) {
-        console.log(response);
-        navigate("/login");
-      })
-      .catch(function (error) {
-        console.log(error.response.data);
-      });
-  };
+  useEffect(() => {
+    const logout = async () => {
+      try {
+        await axios
+          .get("/api/logout")
+          .then(function (response) {
+            console.log(response);
+            navigate("/login");
+            setLoggedIn(false);
+          })
+          .catch(function (error) {
+            console.log(error.response.data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    logout();
+  }, [navigate]);
+
   return <div>LogoutPage</div>;
 };
 
