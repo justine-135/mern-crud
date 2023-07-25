551ca01f0baa421b94d6dbb3d07f0873b0cf351a
Justine <ujusine@yahoo.com>
2023-07-25 16:41:45 +0800

    add relation to user

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
 const PostsSchema = mongoose.Schema(
   {
-    id: { type: ObjectId },
     title: { type: String, required: [true, "Please enter title"] },
     body: { type: String, required: [true, "Please enter body"] },
     category: [{ type: String, required: [true, "Please enter category"] }],
     likes: { type: Number, default: 0 },
     tags: [{ type: String, required: [true, "Please enter tag"] }],
+    user: [{ type: mongoose.Schema.Types.ObjectId, ref: "Users" }],
+    email: [{ type: String, ref: "Users" }],
   },
   { timestamps: true }
 );
