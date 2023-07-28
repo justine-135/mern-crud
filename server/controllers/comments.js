95e46224bd6ec78c3c765e6b779c05a4170ad073
Justine <ujusine@yahoo.com>
2023-07-28 13:53:29 +0800

    remove comment

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -30,7 +30,23 @@ const indexPost = async (req, res) => {
   }
 };
 
+const destroy = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comment = await Comments.deleteOne({ post: id });
+    if (!comment) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any posts with ID ${id}` });
+    }
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   create,
   indexPost,
+  destroy,
 };
